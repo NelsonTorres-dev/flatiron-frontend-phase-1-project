4d3e89cfae07c8b7181222571dad2b12b70f2fec
Nelson Torres <ntorres381@gmail.com>
2023-06-09 11:59:48 -0700

    renders meal category

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # flatiron-frontend-phase-1-project
 My Flatiron Frontend Phase 1 Project
+
+
+things to include in readme: 
+project name 
+description 
+installation: on how to install and setup your project ex: starting json server, doing onpen index html
+how to use your app
+contributor guide also not accepting or looking for contributions
--- main.js
+++ main.js
@@ -2,25 +2,52 @@ console.log('main.js connected')
 
 const searchTermsInput = document.body.querySelector('#search-terms')
 
+
+
 const getMealCategories = async () => {
     const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
 
     try {
         const response = await fetch(mealCategoriesApiURL)
-        const data = response.json()
-        console.log('data', data)
+        const data = await response.json()
+        const categories = await data.categories
+        console.log('categories', categories)
+        return categories; 
     } catch (error){
         console.log(error)
         alert('Something went wrong, try again later')
     }
- 
+   
 }
+//render meal categories data to dom
+const renderMealCategories = (mealCategoriesObj) => {
+    console.log('renderMealCategories')
+    console.table(mealCategoriesObj)
 
+    //find and select a dom element to append my data into 
+
+    //for each element inour meal categories array
+    //create a wrapping element maybe <article> class of card
+    //append the article.card to dom
+    //display the category name, image, and description
+        //image
+            //create an img element
+            //set img src to category thumbnail url
+            //give it a mobile friendly max width of like 300px
+            //appendChild the image to our card
+        //name
+            //create h4 element
+            //set h4 element textContent to be meal category  name
+        //description
+        //create a p element
+            //set p element textContent to be meal category descripton
+}
+//function to call when form input is given focus
 const handleFormInputFocus = () => {
     console.log(`focus occurred`)
 
-    getMealCategories()
+   getMealCategories()
 }
 
-
+//add event listener to search term input
 searchTermsInput.addEventListener('focus', handleFormInputFocus)
