a04de1d14bdc20dc04e8f3b41a56d423103ebf74
Nelson Torres <ntorres381@gmail.com>
2023-06-09 11:31:18 -0700

    adds fetch

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 console.log('main.js connected')
 
-const handleFormInputFocus = () => {
-    console.log(`focus occurred`)
+const searchTermsInput = document.body.querySelector('#search-terms')
+
+const getMealCategories = async () => {
+    const mealCategoriesApiURL = "https://www.themealdb.com/api/json/v1/1/categories.php"
+
+    try {
+        const response = await fetch(mealCategoriesApiURL)
+        const data = response.json()
+        console.log('data', data)
+    } catch (error){
+        console.log(error)
+        alert('Something went wrong, try again later')
+    }
+ 
 }
 
-const searchTermsInput = document.querySelector('#search-terms')
-console.log(searchTermsInput)
-document.addEventListener('focus', handleFormInputFocus)
+const handleFormInputFocus = () => {
+    console.log(`focus occurred`)
+
+    getMealCategories()
+}
+
+
+searchTermsInput.addEventListener('focus', handleFormInputFocus)
