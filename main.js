cb49613866814e03a182af093bcc989c75690f62
Nelson Torres <ntorres381@gmail.com>
2023-06-09 10:34:48 -0700

    adds focus event listener

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('connected')
+console.log('main.js connected')
+
+const handleFormInputFocus = () => {
+    console.log(`focus occurred`)
+}
+
+const searchTermsInput = document.querySelector('#search-terms')
+console.log(searchTermsInput)
+document.addEventListener('focus', handleFormInputFocus)
